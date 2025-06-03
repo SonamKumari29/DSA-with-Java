31b54caf6f61c7e86f97a1ba17c614348d971f34
SonamKumari29 <sonam132btcsai22@igdtuw.ac.in>
2025-06-03 18:15:50 +0530

    day3 graphs

---
+++ graphs/matrix01.java
@@ -0,0 +1,61 @@
+import java.util.*;
+//bfs approach
+class matrix01 {
+    public int[][] updateMatrix(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        int[][] res = new int[m][n];
+        boolean[][] visited = new boolean[m][n];
+        Queue<int[]> queue = new LinkedList<>();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) {
+                    queue.offer(new int[]{i, j});
+                    visited[i][j] = true;
+                }
+            }
+        }
+
+        int[][] directions = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+
+        while (!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            int row = cell[0];
+            int col = cell[1];
+
+            for (int[] dir : directions) {
+                int r = row + dir[0];
+                int c = col + dir[1];
+
+                if (r >= 0 && r < m && c >= 0 && c < n && !visited[r][c]) {
+                    res[r][c] = res[row][col] + 1;
+                    visited[r][c] = true;
+                    queue.offer(new int[]{r, c});
+                }
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        matrix01 sol = new matrix01();
+
+        int[][] mat = {
+            {0, 0, 0},
+            {0, 1, 0},
+            {1, 1, 1}
+        };
+
+        int[][] result = sol.updateMatrix(mat);
+
+        for (int[] row : result) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ graphs/surroundedRegions.java
@@ -0,0 +1,57 @@
+class surroundedRegions {
+    public void solve(char[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'O') dfs(board, i, 0);
+            if (board[i][n - 1] == 'O') dfs(board, i, n - 1);
+        }
+
+        for (int j = 0; j < n; j++) {
+            if (board[0][j] == 'O') dfs(board, 0, j);
+            if (board[m - 1][j] == 'O') dfs(board, m - 1, j);
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') board[i][j] = 'X';
+                else if (board[i][j] == 'T') board[i][j] = 'O';
+            }
+        }
+    }
+
+    private void dfs(char[][] board, int i, int j) {
+        int m = board.length;
+        int n = board[0].length;
+
+        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != 'O') return;
+
+        board[i][j] = 'T';
+
+        dfs(board, i + 1, j);
+        dfs(board, i - 1, j);
+        dfs(board, i, j + 1);
+        dfs(board, i, j - 1);
+    }
+
+    public static void main(String[] args) {
+        surroundedRegions sol = new surroundedRegions();
+
+        char[][] board = {
+            {'X', 'X', 'X', 'X'},
+            {'X', 'O', 'O', 'X'},
+            {'X', 'X', 'O', 'X'},
+            {'X', 'O', 'X', 'X'}
+        };
+
+        sol.solve(board);
+
+        for (char[] row : board) {
+            for (char c : row) {
+                System.out.print(c + " ");
+            }
+            System.out.println();
+        }
+    }
+}
