4c3940d34c1504491fe9fd40efdc874f1ff273a6
SonamKumari29 <sonam132btcsai22@igdtuw.ac.in>
2025-06-09 19:56:51 +0530

    num of provinces

--- graphs/numOfProvinces.java
+++ graphs/numOfProvinces.java
@@ -1,11 +1,12 @@
 import java.util.ArrayList;
 
 class numOfProvinces {
-    private static void dfs(int node, ArrayList<ArrayList<Integer>> adjLs, int vis[]) {
+    // Standard static DFS function (renamed to match OG DFS function naming)
+    static void dfsOfGraph(int node, ArrayList<ArrayList<Integer>> adjLs, int vis[]) {
         vis[node] = 1; 
-        for (Integer it : adjLs.get(node)) {
-            if (vis[it] == 0) {
-                dfs(it, adjLs, vis); 
+        for (int neighbor : adjLs.get(node)) {
+            if (vis[neighbor] == 0) {
+                dfsOfGraph(neighbor, adjLs, vis);
             }
         }
     }
@@ -27,7 +28,7 @@ class numOfProvinces {
         for (int i = 0; i < V; i++) {
             if (vis[i] == 0) {
                 cnt++;
-                dfs(i, adjLs, vis); 
+                dfsOfGraph(i, adjLs, vis); 
             }
         }
         return cnt; 
@@ -54,18 +55,3 @@ class numOfProvinces {
         System.out.println(numOfProvinces.numProvinces(adj, 3));
     }
 }
-
-/*
-Algorithm Flow:
-1. Convert adjacency matrix to adjacency list.
-2. For each unvisited node, call DFS.
-3. Each DFS marks a whole province.
-4. Count total DFS calls = total provinces.
-
-Time Complexity: O(V + E)
-    - V = number of nodes
-    - E = number of edges in the graph
-
-Space Complexity: O(V)
-    - Visited array + recursion stack in DFS
-*/
