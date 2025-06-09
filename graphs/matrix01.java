fac87518a752fa6f35c9aeb91e6b158fb5271d7f
SonamKumari29 <sonam132btcsai22@igdtuw.ac.in>
2025-06-09 20:46:43 +0530

    01 matrix

--- graphs/matrix01.java
+++ graphs/matrix01.java
@@ -1,57 +1,58 @@
-import java.util.*;
+import java.util.*; // import utilities
+
 //bfs approach
 class matrix01 {
     public int[][] updateMatrix(int[][] mat) {
-        int m = mat.length;
-        int n = mat[0].length;
+        int m = mat.length; // rows
+        int n = mat[0].length; // cols
 
-        int[][] res = new int[m][n];
-        boolean[][] visited = new boolean[m][n];
-        Queue<int[]> queue = new LinkedList<>();
+        int[][] res = new int[m][n]; // result matrix
+        boolean[][] visited = new boolean[m][n]; // visited tracker
+        Queue<int[]> queue = new LinkedList<>(); // BFS queue
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] == 0) {
-                    queue.offer(new int[]{i, j});
-                    visited[i][j] = true;
+        for (int i = 0; i < m; i++) { // loop rows
+            for (int j = 0; j < n; j++) { // loop cols
+                if (mat[i][j] == 0) { // if cell is 0
+                    queue.offer(new int[]{i, j}); // add to queue
+                    visited[i][j] = true; // mark visited
                 }
             }
         }
 
-        int[][] directions = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+        int[][] directions = {{0,1}, {1,0}, {0,-1}, {-1,0}}; // right, down, left, up
 
-        while (!queue.isEmpty()) {
-            int[] cell = queue.poll();
-            int row = cell[0];
-            int col = cell[1];
+        while (!queue.isEmpty()) { // while queue not empty
+            int[] cell = queue.poll(); // get cell
+            int row = cell[0]; // row index
+            int col = cell[1]; // col index
 
-            for (int[] dir : directions) {
-                int r = row + dir[0];
-                int c = col + dir[1];
+            for (int[] dir : directions) { // for each direction
+                int r = row + dir[0]; // new row
+                int c = col + dir[1]; // new col
 
-                if (r >= 0 && r < m && c >= 0 && c < n && !visited[r][c]) {
-                    res[r][c] = res[row][col] + 1;
-                    visited[r][c] = true;
-                    queue.offer(new int[]{r, c});
+                if (r >= 0 && r < m && c >= 0 && c < n && !visited[r][c]) { // valid and not visited
+                    res[r][c] = res[row][col] + 1; // set distance
+                    visited[r][c] = true; // mark visited
+                    queue.offer(new int[]{r, c}); // add to queue
                 }
             }
         }
 
-        return res;
+        return res; // return result
     }
 
     public static void main(String[] args) {
-        matrix01 sol = new matrix01();
+        matrix01 sol = new matrix01(); // create object
 
         int[][] mat = {
             {0, 0, 0},
             {0, 1, 0},
             {1, 1, 1}
-        };
+        }; // input matrix
 
-        int[][] result = sol.updateMatrix(mat);
+        int[][] result = sol.updateMatrix(mat); // call function
 
-        for (int[] row : result) {
+        for (int[] row : result) { // print result
             for (int val : row) {
                 System.out.print(val + " ");
             }
