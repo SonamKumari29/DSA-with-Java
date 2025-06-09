4511f874be4bc8106fa6edbf6b578a71cc5f8de0
SonamKumari29 <sonam132btcsai22@igdtuw.ac.in>
2025-06-09 20:48:09 +0530

    surrounded regions

--- graphs/surroundedRegions.java
+++ graphs/surroundedRegions.java
@@ -1,38 +1,41 @@
 class surroundedRegions {
     public void solve(char[][] board) {
-        int m = board.length;
-        int n = board[0].length;
+        int m = board.length; // number of rows
+        int n = board[0].length; // number of columns
 
+        // Mark border 'O's and their connected 'O's
         for (int i = 0; i < m; i++) {
-            if (board[i][0] == 'O') dfs(board, i, 0);
-            if (board[i][n - 1] == 'O') dfs(board, i, n - 1);
+            if (board[i][0] == 'O') dfs(board, i, 0); // left border
+            if (board[i][n - 1] == 'O') dfs(board, i, n - 1); // right border
         }
 
         for (int j = 0; j < n; j++) {
-            if (board[0][j] == 'O') dfs(board, 0, j);
-            if (board[m - 1][j] == 'O') dfs(board, m - 1, j);
+            if (board[0][j] == 'O') dfs(board, 0, j); // top border
+            if (board[m - 1][j] == 'O') dfs(board, m - 1, j); // bottom border
         }
 
+        // Flip surrounded 'O's to 'X', revert 'T' to 'O'
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (board[i][j] == 'O') board[i][j] = 'X';
-                else if (board[i][j] == 'T') board[i][j] = 'O';
+                if (board[i][j] == 'O') board[i][j] = 'X'; // surrounded region
+                else if (board[i][j] == 'T') board[i][j] = 'O'; // border-connected region
             }
         }
     }
 
     private void dfs(char[][] board, int i, int j) {
-        int m = board.length;
-        int n = board[0].length;
+        int m = board.length; // rows
+        int n = board[0].length; // cols
 
+        // Out of bounds or not 'O'
         if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != 'O') return;
 
-        board[i][j] = 'T';
+        board[i][j] = 'T'; // Mark as visited (temporary)
 
-        dfs(board, i + 1, j);
-        dfs(board, i - 1, j);
-        dfs(board, i, j + 1);
-        dfs(board, i, j - 1);
+        dfs(board, i + 1, j); // down
+        dfs(board, i - 1, j); // up
+        dfs(board, i, j + 1); // right
+        dfs(board, i, j - 1); // left
     }
 
     public static void main(String[] args) {
@@ -49,9 +52,9 @@ class surroundedRegions {
 
         for (char[] row : board) {
             for (char c : row) {
-                System.out.print(c + " ");
+                System.out.print(c + " "); // print each cell
             }
-            System.out.println();
+            System.out.println(); // new line after each row
         }
     }
 }
